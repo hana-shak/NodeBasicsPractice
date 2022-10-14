9db98c64b20a8de49f8d36f032b6e8f43157e86c
Hana <hanashakboua@gmail.com>
2022-10-14 07:18:13 +0300

    before re-create model

--- util/dbsql.js
+++ util/dbsql.js
@@ -1,4 +1,5 @@
-const mysql = require('mysql2');
+/* For SQL intro
+ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host : 'localhost',
@@ -8,3 +9,20 @@ const pool = mysql.createPool({
 })
 
 module.exports = pool.promise(); 
+*/
+
+/* For Seqeulize */
+
+const Seqeulize = require('seqeulize');
+
+const seqeulize = new Seqeulize(
+    'nodeComplete',
+    'root',
+    'mysql12345//',
+    {
+        dialect : 'mysql',
+        host : 'localhost'
+    }
+);
+
+module.exports = seqeulize; 
